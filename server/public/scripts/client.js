1d7a39cc6882dd8c87a65033e98e73cf4618da54
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-19 09:57:09 -0500

    interface and client side started

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,25 @@
-console.log('js');
+$(document).ready( readyNow)
 
-$(document).ready( function(){
-    console.log('jQuery Loaded');
-});
+function readyNow(){
+    $('#clearButton').on('click', function () {
+    });//end clearButton
+
+    $('#doMathButton').on('click', function(){
+        console.log('in doMathButton');
+        //capture inputs
+        // let x= $("#xIn").val()
+        // let y = $("#yIn").val()
+        // let type = $("#typeIn").val()
+        //store in an object
+        let objectToSend = {
+            x: $("#xIn").val(),
+            y: $("#yIn").val(),
+            type: $("#typeIn").val()
+        }
+        console.log(objectToSend);
+        
+        //send object to server with AJAX
+        //display answer on DOM
+    });//end doMathButton on click
+}
 
